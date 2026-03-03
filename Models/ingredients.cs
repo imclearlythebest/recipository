ea28301f17057d077cb000c92561312a3a8af612
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2026-03-03 16:11:02 +0600

    Create dummy file for ingredient class

---
+++ Models/ingredients.cs
@@ -0,0 +1,7 @@
+public class ingredients
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string description { get; set; }
+    public string image { get; set; }
+}
