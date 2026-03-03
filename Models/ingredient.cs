e5281c5d9f925e66d0293edc5b971508d798ba24
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2026-03-03 20:06:53 +0600

    Rename file and variables to match naming conventions

---
+++ Models/ingredient.cs
@@ -0,0 +1,6 @@
+public class Ingredient
+{
+    public int ID { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+}
--- Models/ingredients.cs
+++
@@ -1,7 +0,0 @@
-public class ingredients
-{
-    public int id { get; set; }
-    public string name { get; set; }
-    public string description { get; set; }
-    public string image { get; set; }
-}
