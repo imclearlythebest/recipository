92e4449d8803a7bc5810203203bc70404d579dc1
Mehbuba1375 <mehbuba.morshed@g.bracu.ac.bd>
2026-03-18 21:14:44 +0600

    Worked on the Shopping Cart Feature and Fixed some other Issues

--- Website/Controllers/CartController.cs
+++ Website/Controllers/CartController.cs
@@ -1,42 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Website.Data;
 using Website.Models;
 
 namespace Website.Controllers;
 
 public class CartController : Controller
 {
-    private static List<CartItem> _cart = new List<CartItem>();
+  private readonly AppDbContext _context;
+  private static List<CartItem> _cart = new List<CartItem>();
 
-    public IActionResult Index()
+  public CartController(AppDbContext context) => _context = context;
+
+  public IActionResult Index() => View(_cart);
+
+  [HttpPost]
+  public async Task<IActionResult> AddToCart(int id)
+  {
+
+    var item = await _context.Ingredients.OfType<ShopItem>().FirstOrDefaultAsync(x => x.Id == id);
+    
+    if (item != null)
     {
-        return View(_cart);
+      var existing = _cart.FirstOrDefault(x => x.ShopItemId == id);
+      if (existing != null) {
+        existing.Quantity += 1;
+      } else {
+        _cart.Add(new CartItem { ShopItemId = id, Product = item, Quantity = 1 });
+      }
     }
 
-    [HttpPost]
-    public IActionResult AddToCart(int id, int quantity = 1)
-    {
-        var existingItem = _cart.FirstOrDefault(x => x.ShopItemId == id);
+    return Ok(); 
+  }
 
-        if (existingItem != null)
-        {
-            existingItem.Quantity += quantity;
-        }
-        else
-        {
+  [HttpPost]
+  public IActionResult Remove(int id)
+  {
+    _cart.RemoveAll(x => x.ShopItemId == id);
 
-            _cart.Add(new CartItem { 
-                ShopItemId = id, 
-                Quantity = quantity,
-                Product = new ShopItem { Name = "Selected Item", Price = 10.00f } 
-            });
-        }
-        return RedirectToAction("Index");
-    }
-
-    [HttpPost]
-    public IActionResult Remove(int id)
-    {
-        _cart.RemoveAll(x => x.ShopItemId == id);
-        return RedirectToAction("Index");
-    }
+    return Content(""); 
+  }
 }
